a6e26cf87387c13d72427c83c4133c613b4c82d9
eddiesri <eddie.sri93@gmail.com>
2018-10-09 21:55:37 -0500

    componentsAndCDN

--- appOLSoftware/src/app/app.module.ts
+++ appOLSoftware/src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
-
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './static-components/header/header.component';
+import { FormsModule} from '@angular/forms';
+import { FooterComponent } from './static-components/footer/footer.component';
+import { HomeComponent } from './dynamic-components/home/home.component';
+import { RegisterComponent } from './dynamic-components/register/register.component';
+import { SidebarComponent } from './static-components/sidebar/sidebar.component';
+import { NotFoundComponent } from './dynamic-components/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent,
+    SidebarComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    FormsModule
   ],
-  providers: [],
+  providers: [AppComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- appOLSoftware/src/environments/environment.ts
+++ appOLSoftware/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyBZcNngav2vFKUeCD_uIbqLVsxWrjCH6A0',
+    authDomain: 'olsoftware-f573a.firebaseapp.com',
+    databaseURL: 'https://olsoftware-f573a.firebaseio.com',
+    projectId: 'olsoftware-f573a',
+    storageBucket: 'olsoftware-f573a.appspot.com',
+    messagingSenderId: '174904418358'
+  }
 };
